493a12262d6fabbc614cbe655e3d38a91e524e5e
galue99 <eespinetti_99@hotmail.com>
2015-04-06 10:09:49 -0430

    26

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
 var express = require('express');
+var app     = express();
+var server  = require('http').createServer(app);
+var io      = require('socket.io').listen(server);
 
-var app = express();
-app.use('/', express.static('app/'));
-app.use('/bower_components', express.static('../bower_components/'));
-
-var http   = require('http').Server(app);
-var io     = require('socket.io')(http);
-
-var bodyParser       = require('body-parser');
-var jsonParser       = bodyParser.json();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 var urlencodedParser = app.use(bodyParser.urlencoded({extended: true}));
 
-var tickets = [];
+var tickets = [
 
+];
 
-app.get('/', function (req, res) {
+app.set('port', (process.env.PORT || 3000));
+app.use('/bower_components', express.static('../bower_components/'));
+app.use(express.static('app/'));
 
+var tickets = [
 
-});
+];
 
 app.get('/tickets', function (req, res) {
     'use strict';
@@ -31,15 +31,12 @@ app.post('/tickets', jsonParser, function (req, res) {
     if (!req.body) {
         return res.sendStatus(400);
     }
-
     tickets.push(req.body);
-    //console.log(req.body);
 
     io.emit('ticket', req.body);
     return res.sendStatus(200);
 });
 
-http.listen(3000, function () {
-    'use strict';
-    console.log('Connect to port 3000');
+server.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
